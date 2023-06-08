52bc53a733df5819eaeb832140ee8273ebc939aa
agallego21 <ag.gallego@gmail.com>
2023-06-08 23:17:27 +0200

    Creación de la estructura del proyecto

---
+++ src/com/unir/isa/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package com.unir.isa.calculadora;
+
+/** 
+ * Clase que implementa una calculadora
+ */
+public class Calculadora {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/com/unir/isa/calculadora/test/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package com.unir.isa.calculadora.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Clase que contiene los Test Unit definidos para el proyecto Calculadora
+ *
+ */
+class CalculadoraTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
