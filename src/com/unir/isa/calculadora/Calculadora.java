b7a064c9bae854c193c5e3b4c485eb83503728d1
agallego21 <ag.gallego@gmail.com>
2023-06-11 17:07:19 +0200

    Función que muestra el menú de la calculadora

--- src/com/unir/isa/calculadora/Calculadora.java
+++ src/com/unir/isa/calculadora/Calculadora.java
@@ -1,16 +1,18 @@
 package com.unir.isa.calculadora;
 
+import java.io.IOException;
+
 /** 
  * Clase que implementa una calculadora
  */
 public class Calculadora {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception{
+		showMenu();
 
 	}
 
-		// Métodos de la calculadora
+	// Métodos de la calculadora
     public static Double suma(double d, double e) {
 
         return d + e;
@@ -26,4 +28,23 @@ public class Calculadora {
 		return i * j;
 	}
 
+    /**
+     * Muestra el menú de la calculadora, borrando el contenido de la pantalla
+     */
+    private static void showMenu() throws InterruptedException, IOException{
+    	//Limpiar pantalla
+    	new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+    	System.out.println("==========================="+
+    			"\n    *** CALCULADORA ***"+
+    			"\n==========================="+
+    			"\nOperaciones disponibles:" + 
+    			"\n  1) Suma" + 
+    			"\n  2) Resta" + 
+    			"\n  3) Multiplicación" + 
+    			"\n  4) División" + 
+    			"\n  5) Raíz cuadrada" + 
+    			"\n\n  0) Salir");
+    	System.out.print("\n\n  Por favor, seleccione la operación deseada:  ");
+    }
 }
